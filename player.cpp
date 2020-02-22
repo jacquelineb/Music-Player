f18fca2c53866b7444ffc38dcc580e8d87734301
jacquelineb <jacquelinesotheremail@gmail.com>
2020-02-21 17:05:20 -0800

    Got rid of the QMediaPlaylist that was used to treat the entire library like a playlist. Instead I just iterate through the libraryProxyModel in order to play the songs of the library. This way I don't have to remake the QMediaPlaylist everytime the user decides to resort the library.
    To do this I had to make changes to the fuctions:
    -updateCurrTrackLabel()
    -updatePlaylistTreeViewSelection()
    
    and added functions to set/play media:
    -playOrPauseMedia()
    -setMediaForPlayback()
    -setNextMediaForPlayback()
    -setPreviousMediaForPlayback()

--- player.cpp
+++ player.cpp
@@ -23,11 +23,10 @@ Player::Player(QWidget *parent) :
     ui->setupUi(this);
     initializeLibraryModels();
     initializeLibraryTreeView();
-    initializeLibraryPlaylist();
     initializeMediaPlayer();
     setUpConnections();
 
-    // Change this later
+    // Change this later. try taglib
     mediaToBeAdded = new QMediaPlayer(this);
     connect(mediaToBeAdded, &QMediaPlayer::mediaStatusChanged, this, &Player::onAddMediaStatusChanged);
 }
@@ -44,49 +43,35 @@ void Player::initializeLibraryModels()
     librarySourceModel->setRelation(2, QSqlRelation("Artist", "id", "name"));
     librarySourceModel->select();
 
-    libraryViewModel = new LibraryPlaylistModel(this);
-    libraryViewModel->setSourceModel(librarySourceModel);
-    libraryViewModel->setHeaderData(0, Qt::Horizontal, tr("Track ID"));
-    libraryViewModel->setHeaderData(1, Qt::Horizontal, tr("Title"));
-    libraryViewModel->setHeaderData(2, Qt::Horizontal, tr("Artist"));
-    libraryViewModel->setHeaderData(3, Qt::Horizontal, tr("Album"));
-    libraryViewModel->setHeaderData(4, Qt::Horizontal, tr("Track Number"));
-    libraryViewModel->setHeaderData(5, Qt::Horizontal, tr("Year"));
-    libraryViewModel->setHeaderData(6, Qt::Horizontal, tr("Genre"));
-    libraryViewModel->setHeaderData(7, Qt::Horizontal, tr("Duration"));
-    libraryViewModel->setHeaderData(8, Qt::Horizontal, tr("Location"));
-    libraryViewModel->setDynamicSortFilter(true);
+    libraryProxyModel = new LibraryPlaylistModel(this);
+    libraryProxyModel->setSourceModel(librarySourceModel);
+    libraryProxyModel->setHeaderData(0, Qt::Horizontal, tr("Track ID"));
+    libraryProxyModel->setHeaderData(1, Qt::Horizontal, tr("Title"));
+    libraryProxyModel->setHeaderData(2, Qt::Horizontal, tr("Artist"));
+    libraryProxyModel->setHeaderData(3, Qt::Horizontal, tr("Album"));
+    libraryProxyModel->setHeaderData(4, Qt::Horizontal, tr("Track Number"));
+    libraryProxyModel->setHeaderData(5, Qt::Horizontal, tr("Year"));
+    libraryProxyModel->setHeaderData(6, Qt::Horizontal, tr("Genre"));
+    libraryProxyModel->setHeaderData(7, Qt::Horizontal, tr("Duration"));
+    libraryProxyModel->setHeaderData(8, Qt::Horizontal, tr("Location"));
+    libraryProxyModel->setDynamicSortFilter(true);
 }
 
 void Player::initializeLibraryTreeView()
 {
-    ui->playlistTreeView->setModel(libraryViewModel);
-    ui->playlistTreeView->sortByColumn(2, Qt::SortOrder::AscendingOrder);
-    /*
-    ui->playlistTreeView->setColumnHidden(0, true);
-    ui->playlistTreeView->setColumnHidden(4, true);
-    ui->playlistTreeView->setColumnHidden(5, true);
-    ui->playlistTreeView->setColumnHidden(8, true);
-    */
+    ui->playlistView->setModel(libraryProxyModel);
+    ui->playlistView->sortByColumn(2, Qt::SortOrder::AscendingOrder);
 
-}
-
-void Player::initializeLibraryPlaylist()
-{
-    playlist = new QMediaPlaylist(this);
-    for (int i = 0; i < libraryViewModel->rowCount(); i++)
-    {
-        QModelIndex index = libraryViewModel->index(i, 8, QModelIndex());
-        QString trackLocation = libraryViewModel->data(index).toString();
-        playlist->addMedia(QUrl::fromLocalFile(trackLocation));
-    }
+    //ui->playlistView->setColumnHidden(0, true);
+    //ui->playlistView->setColumnHidden(4, true);
+    //ui->playlistView->setColumnHidden(5, true);
+    //ui->playlistView->setColumnHidden(8, true);
 }
 
 void Player::initializeMediaPlayer()
 {
     mediaPlayer = new QMediaPlayer(this);
     restorePlayerSettings();
-    mediaPlayer->setPlaylist(playlist);
 }
 
 void Player::restorePlayerSettings()
@@ -97,26 +82,112 @@ void Player::restorePlayerSettings()
 
 void Player::setUpConnections()
 {
+    connect(ui->playlistView, &QTreeView::doubleClicked, this, &Player::setMediaForPlayback);
+
     connect(mediaPlayer, &QMediaPlayer::mediaStatusChanged, this, &Player::onStatusChanged);
     connect(mediaPlayer, &QMediaPlayer::stateChanged, ui->controls, &PlayerControls::setPlayButtonLabel); // maybe just rename setPlayButtonLabel to setControlState
     connect(mediaPlayer, &QMediaPlayer::durationChanged, ui->controls, &PlayerControls::setupProgressSlider);
     connect(mediaPlayer, &QMediaPlayer::positionChanged, ui->controls, &PlayerControls::updateProgressSlider);
-
-    connect(playlist, &QMediaPlaylist::currentMediaChanged, this, &Player::updateCurrTrackLabel);
-    connect(playlist, &QMediaPlaylist::currentMediaChanged, this, &Player::updatePlaylistTreeViewSelection);
-
-    connect(ui->playlistTreeView, &QTreeView::doubleClicked, this, &Player::playDoubleClickedTrack);
+    connect(mediaPlayer, &QMediaPlayer::currentMediaChanged, this, &Player::updateCurrTrackLabel);
+    connect(mediaPlayer, &QMediaPlayer::currentMediaChanged, this, &Player::updatePlaylistTreeViewSelection); // Check difference between QMediaPlayer's mediChanged and currentMediaChanged signals
 
     connect(ui->controls, &PlayerControls::volumeChanged, mediaPlayer, &QMediaPlayer::setVolume);
     connect(ui->controls, &PlayerControls::progressSliderMoved, mediaPlayer, &QMediaPlayer::setPosition);
     connect(ui->controls, &PlayerControls::playOrPauseClicked, this, &Player::playOrPauseMedia);
-    connect(ui->controls, &PlayerControls::prevClicked, playlist, &QMediaPlaylist::previous);
-    connect(ui->controls, &PlayerControls::nextClicked, playlist, &QMediaPlaylist::next);
+    connect(ui->controls, &PlayerControls::nextClicked, this, &Player::setNextMediaForPlayback);
+    connect(ui->controls, &PlayerControls::prevClicked, this, &Player::setPreviousMediaForPlayback);
+}
+
+void Player::setMediaForPlayback(const QModelIndex &selectedIndex)
+{
+    if (selectedIndex.isValid())
+    {
+        srcIndexOfCurrMedia = libraryProxyModel->mapToSource(selectedIndex);
+        QUrl trackLocation = libraryProxyModel->data(libraryProxyModel->index(selectedIndex.row(), 8, QModelIndex())).toUrl();
+        mediaPlayer->setMedia(trackLocation);
+    }
+    else
+    {
+        srcIndexOfCurrMedia = QModelIndex();
+        mediaPlayer->setMedia(QMediaContent());
+    }
+}
+
+void Player::onStatusChanged(QMediaPlayer::MediaStatus status)
+{
+    if (status == QMediaPlayer::LoadedMedia)
+    {
+        playLoadedMedia();
+    }
+    else if (status == QMediaPlayer::EndOfMedia)
+    {
+        setNextMediaForPlayback();
+    }
+    else if (status == QMediaPlayer::InvalidMedia)
+    {
+        qDebug() << "Line 282: " << status;
+        qDebug() << "Error loading file";
+        // error checking?
+        // send a signal to be recieved by MainWindow about any errors (use mediaPlayer->error())
+        // As example of when this status might occur is if a media file couldn't be located, eg., if I deleted an added song file from disk.
+    }
+    else
+    {
+        qDebug() << "Line 294: " << status;
+    }
+}
+
+void Player::playLoadedMedia()
+{
+    mediaPlayer->play();
+}
+
+void Player::setNextMediaForPlayback()
+{
+    QModelIndex proxyIndexOfCurrMedia = libraryProxyModel->mapFromSource(srcIndexOfCurrMedia);
+    QModelIndex next = proxyIndexOfCurrMedia.siblingAtRow(proxyIndexOfCurrMedia.row()+1);
+    setMediaForPlayback(next);
+}
+
+void Player::setPreviousMediaForPlayback()
+{
+    QModelIndex proxyIndexOfCurrMedia = libraryProxyModel->mapFromSource(srcIndexOfCurrMedia);
+    QModelIndex previous = proxyIndexOfCurrMedia.siblingAtRow(proxyIndexOfCurrMedia.row()-1);
+    setMediaForPlayback(previous);
+}
+
+void Player::updateCurrTrackLabel()
+{
+    QModelIndex proxyIndexOfCurrMedia = libraryProxyModel->mapFromSource(srcIndexOfCurrMedia);
+    if (proxyIndexOfCurrMedia.isValid())
+    {
+        int currRow = proxyIndexOfCurrMedia.row();
+        QString currTrack = libraryProxyModel->data(libraryProxyModel->index(currRow, 1)).toString();
+        QString currArtist = libraryProxyModel->data(libraryProxyModel->index(currRow, 2)).toString();
+        qDebug() << currTrack << " " << currArtist;
+        QString currentlyPlayingText = "Now Playing: ";
+        if (!currArtist.isEmpty())
+        {
+            currentlyPlayingText += currArtist + " - ";
+        }
+        currentlyPlayingText += currTrack;
+        ui->currentTrackLabel->setText(currentlyPlayingText);
+    }
+    else
+    {
+        ui->currentTrackLabel->clear();
+    }
+}
+
+void Player::updatePlaylistTreeViewSelection()
+{
+    // Automatically highlight the currently playing track in the playlist view.
+    QModelIndex proxyIndexOfCurrMedia = libraryProxyModel->mapFromSource(srcIndexOfCurrMedia);
+    ui->playlistView->setCurrentIndex(proxyIndexOfCurrMedia);
 }
 
 void Player::playOrPauseMedia()
 {
-    qDebug() << "Playorpause";
     if (mediaPlayer->state() == QMediaPlayer::State::PlayingState)
     {
         mediaPlayer->pause();
@@ -129,25 +200,20 @@ void Player::playOrPauseMedia()
              * start playback from the first highlighted song if there is one.
              * Otherwise just start playback from the beginning of playlist.
             */
-            int highlightedRow = ui->playlistTreeView->currentIndex().row();
-            if (highlightedRow != -1)
+            QModelIndex selectedIndex = ui->playlistView->currentIndex();
+            if (selectedIndex.isValid())
             {
-                playlist->setCurrentIndex(highlightedRow);
+                setMediaForPlayback(selectedIndex);
             }
             else
             {
-                playlist->setCurrentIndex(0);
+                setMediaForPlayback(libraryProxyModel->index(0, 0, QModelIndex()));
             }
         }
         mediaPlayer->play();
     }
 }
 
-void Player::playDoubleClickedTrack(const QModelIndex &index)
-{
-    playlist->setCurrentIndex(index.row());
-    mediaPlayer->play();
-}
 
 void Player::savePlayerSettings()
 {
@@ -214,11 +280,6 @@ void Player::insertToTrackTable(QString const& title, int artistId, QString cons
         qDebug() << "Line 211: Succesful exec\n";
         librarySourceModel->select();
 
-        // Update the playlist with the new track.
-        QModelIndex srcIndexOfNewTrack = librarySourceModel->index(librarySourceModel->rowCount()-1, 0, QModelIndex());
-        QModelIndex viewIndexOfNewTrack = libraryViewModel->mapFromSource(srcIndexOfNewTrack);
-        playlist->insertMedia(viewIndexOfNewTrack.row(), QUrl::fromLocalFile(location));
-
         /* There is currently a bug where if a song in the playlist is already playing and the user decides to add
          * a new song to the library, if the new song was inserted into the playlist at some index before the current song,
          * playback will start at the beginning of the playlist. If the new song was inserted into the playlist somewhere
@@ -273,70 +334,3 @@ void Player::onAddMediaStatusChanged(QMediaPlayer::MediaStatus status)
     }
 }
 
-void Player::onStatusChanged(QMediaPlayer::MediaStatus status)
-{
-    if (status == QMediaPlayer::LoadedMedia) // Once the media is loaded, i want it to start playing.
-    {
-        qDebug() << "Media has loaded";
-        //mediaPlayer->play();
-    }
-    else if (status == QMediaPlayer::InvalidMedia)
-    {
-        qDebug() << "Line 282: " << status;
-        qDebug() << "Error loading file";
-        // error checking?
-        // send a signal to be recieved by MainWindow about any errors (use mediaPlayer->error())
-        // As example of when this status might occur is if a media file couldn't be located, eg., if I deleted an added song file from disk.
-    }
-    else if (status == QMediaPlayer::LoadingMedia)
-    {
-        qDebug() << "LOading media..";
-    }
-    else if (status == QMediaPlayer::BufferedMedia)
-    {
-        qDebug() << "Buffered Media...";
-    }
-    else if (status == QMediaPlayer::NoMedia)
-    {
-        //ui->currentTrackLabel->clear();
-    }
-    else if (status == QMediaPlayer::EndOfMedia)
-    {
-        qDebug() << status;
-        ui->currentTrackLabel->clear();
-    }
-    else
-    {
-        qDebug() << "Line 294: " << status;
-    }
-}
-
-void Player::updateCurrTrackLabel()
-{
-    int currPlaylistIndex = playlist->currentIndex();
-    qDebug() << currPlaylistIndex;
-    if (currPlaylistIndex != -1)
-    {
-        QString currTrack = libraryViewModel->data(libraryViewModel->index(currPlaylistIndex, 1)).toString();
-        QString currArtist = libraryViewModel->data(libraryViewModel->index(currPlaylistIndex, 2)).toString();
-        qDebug() << currTrack << " " << currArtist;
-        QString currentlyPlayingText = "Now Playing: ";
-        if (!currArtist.isEmpty())
-        {
-            currentlyPlayingText += currArtist + " - ";
-        }
-        currentlyPlayingText += currTrack;
-        ui->currentTrackLabel->setText(currentlyPlayingText);
-    }
-    else
-    {
-        ui->currentTrackLabel->clear();
-    }
-}
-
-void Player::updatePlaylistTreeViewSelection()
-{
-    int currPlaylistIndex = playlist->currentIndex();
-    QModelIndex index = libraryViewModel->index(currPlaylistIndex, 0);
-    ui->playlistTreeView->setCurrentIndex(index);
-}
--- player.h
+++ player.h
@@ -24,16 +24,15 @@ public:
 private:
     Ui::Player *ui;
     QSqlRelationalTableModel *librarySourceModel = nullptr;
-    LibraryPlaylistModel *libraryViewModel = nullptr;
+    LibraryPlaylistModel *libraryProxyModel = nullptr;
     QMediaPlayer *mediaPlayer = nullptr;
     QMediaPlayer *mediaToBeAdded = nullptr;
-    QMediaPlaylist *playlist = nullptr;
     qint64 position_ = 0;
     QSettings settings{"session.ini", QSettings::Format::IniFormat};
+    QModelIndex srcIndexOfCurrMedia;
 
     void closeEvent(QCloseEvent *event);
     void initializeLibraryModels();
-    void initializeLibraryPlaylist();
     void initializeLibraryTreeView();
     void initializeMediaPlayer();
     void insertToTrackTable(QString const& title, int artistId, QString const& album,
@@ -47,10 +46,13 @@ private:
 private slots:
     void onStatusChanged(QMediaPlayer::MediaStatus status);
     void onAddMediaStatusChanged(QMediaPlayer::MediaStatus status);
-    void playDoubleClickedTrack(const QModelIndex &index);
+    void setMediaForPlayback(const QModelIndex &index);
     void playOrPauseMedia();
     void updateCurrTrackLabel();
     void updatePlaylistTreeViewSelection();
+    void setNextMediaForPlayback();
+    void setPreviousMediaForPlayback();
+    void playLoadedMedia();
 };
 
 #endif // PLAYER_H
